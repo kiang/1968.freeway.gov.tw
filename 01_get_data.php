27460ed17a1709b47ba68b69ce208e5f7f7c5377
kiang <kiange@gmail.com>
2020-04-29 15:55:53 +0800

    remove unused fields

--- 01_get_data.php
+++ 01_get_data.php
@@ -1,3 +1,8 @@
 <?php
 $json = json_decode(file_get_contents('https://1968.freeway.gov.tw/api/getCrowdData?action=crowd&area=B'));
+foreach($json->response AS $k => $r) {
+    unset($json->response[$k]->id);
+    unset($json->response[$k]->created_at);
+    unset($json->response[$k]->updated_at);
+}
 file_put_contents(__DIR__ . '/crowd.json', json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
